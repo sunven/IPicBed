bf436f6c6c396d854057c34509fda583083624b5
seven <604164227@qq.com>
2017-12-17 20:48:30 +0800

    添加项目

---
+++ IPicBed/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace IPicBed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.七牛配置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.radMarkdown = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelectPicture = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTip = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.七牛配置ToolStripMenuItem,
+            this.关于ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(375, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 七牛配置ToolStripMenuItem
+            // 
+            this.七牛配置ToolStripMenuItem.Name = "七牛配置ToolStripMenuItem";
+            this.七牛配置ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.七牛配置ToolStripMenuItem.Text = "七牛配置";
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.关于ToolStripMenuItem.Text = "关于";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.radioButton2);
+            this.splitContainer1.Panel1.Controls.Add(this.radMarkdown);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.AllowDrop = true;
+            this.splitContainer1.Panel2.Controls.Add(this.lblTip);
+            this.splitContainer1.Panel2.Controls.Add(this.btnSelectPicture);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.txtUrl);
+            this.splitContainer1.Panel2.DragDrop += new System.Windows.Forms.DragEventHandler(this.splitContainer1_Panel2_DragDrop);
+            this.splitContainer1.Panel2.DragEnter += new System.Windows.Forms.DragEventHandler(this.splitContainer1_Panel2_DragEnter);
+            this.splitContainer1.Size = new System.Drawing.Size(375, 305);
+            this.splitContainer1.SplitterDistance = 25;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // radMarkdown
+            // 
+            this.radMarkdown.AutoSize = true;
+            this.radMarkdown.Checked = true;
+            this.radMarkdown.Location = new System.Drawing.Point(86, 4);
+            this.radMarkdown.Name = "radMarkdown";
+            this.radMarkdown.Size = new System.Drawing.Size(119, 16);
+            this.radMarkdown.TabIndex = 0;
+            this.radMarkdown.TabStop = true;
+            this.radMarkdown.Text = "返回Markdown格式";
+            this.radMarkdown.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(211, 4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(65, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "返回url";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(4, 4);
+            this.txtUrl.Multiline = true;
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(368, 63);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "拖拽图片到此区域以上传或";
+            // 
+            // btnSelectPicture
+            // 
+            this.btnSelectPicture.Location = new System.Drawing.Point(151, 136);
+            this.btnSelectPicture.Name = "btnSelectPicture";
+            this.btnSelectPicture.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectPicture.TabIndex = 2;
+            this.btnSelectPicture.Text = "选择图片";
+            this.btnSelectPicture.UseVisualStyleBackColor = true;
+            this.btnSelectPicture.Click += new System.EventHandler(this.btnSelectPicture_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "上传成功后：";
+            // 
+            // lblTip
+            // 
+            this.lblTip.AutoSize = true;
+            this.lblTip.ForeColor = System.Drawing.Color.Red;
+            this.lblTip.Location = new System.Drawing.Point(12, 255);
+            this.lblTip.Name = "lblTip";
+            this.lblTip.Size = new System.Drawing.Size(0, 12);
+            this.lblTip.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 330);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "i图床";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 七牛配置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radMarkdown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSelectPicture;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label lblTip;
+    }
+}
+
---
+++ IPicBed/Form1.cs
@@ -0,0 +1,119 @@
+﻿using Qiniu.Http;
+using Qiniu.Storage;
+using Qiniu.Util;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace IPicBed
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void splitContainer1_Panel2_DragDrop(object sender, DragEventArgs e)
+        {
+            var files = e.Data.GetData(DataFormats.FileDrop, false) as String[];
+            Upload(files);
+        }
+
+        private void InitControl()
+        {
+            lblTip.Text = "";
+            txtUrl.Text = "";
+        }
+
+        private void Upload(string[] files)
+        {
+            if (files == null)
+            {
+                return;
+            }
+            InitControl();
+            var urlList = new List<string>();
+            foreach (var item in files)
+            {
+                if (Directory.Exists(item))
+                {
+                    continue;
+                }
+                var url = Upload(files[0]);
+                urlList.Add(url);
+
+            }
+            if (radMarkdown.Checked)
+            {
+                urlList = urlList.Select(c => $"![image]({c})").ToList();
+            }
+
+            var urlStrs= string.Join(",", urlList);
+            Clipboard.SetDataObject(urlStrs);
+            txtUrl.Text = urlStrs;
+            lblTip.Text = "上传成功！已复制到剪贴板";
+        }
+
+        private string Upload(string path)
+        {
+            var AccessKey = "VuozjapP1huZqCkDIIiRPhy9At2GsYvJzvscpg13";
+            var SecretKey = "x1-dw8nJL6OY8BIzF6-a4evbVRGAa3kmvPIg2Scq";
+            var Bucket = "v-video";
+            var Domain = "http://7xk2dp.com1.z0.glb.clouddn.com";
+            Mac mac = new Mac(AccessKey, SecretKey);
+            Random rand = new Random();
+            var fileName = Path.GetFileNameWithoutExtension(path) + DateTime.Now.ToString("yyyyMMddHHmmssffff")+ Path.GetExtension(path);
+            //string key = string.Format("UploadFileTest_{0}.dat", rand.Next());
+
+            //string filePath = LocalFile;
+
+            PutPolicy putPolicy = new PutPolicy();
+            putPolicy.Scope = Bucket + ":" + fileName;
+            putPolicy.SetExpires(3600);
+            putPolicy.DeleteAfterDays = 1;
+            string token = Auth.CreateUploadToken(mac, putPolicy.ToJsonString());
+            Config config = new Config();
+            config.Zone = Zone.ZONE_CN_East;
+            config.UseHttps = true;
+            config.UseCdnDomains = true;
+            config.ChunkSize = ChunkUnit.U512K;
+            FormUploader target = new FormUploader(config);
+            HttpResult result = target.UploadFile(path, fileName, token, null);
+            Console.WriteLine("form upload result: " + result.ToString());
+            if ((int)HttpCode.OK==result.Code)
+            {
+                return Domain + "/" + fileName;
+            }
+            return null;
+        }
+
+        private void btnSelectPicture_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog
+            {
+                Filter = "All Image Files|*.bmp;*.ico;*.gif;*.jpeg;*.jpg;*.png;",
+              Multiselect=true
+            };
+            if (ofd.ShowDialog()==DialogResult.OK)
+            {
+                Upload(ofd.FileNames);
+            }
+        }
+    }
+}
